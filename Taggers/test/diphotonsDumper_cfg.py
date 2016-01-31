c042f696c6e122ba3ef5f996144e2402da04c786
Federico Ferri <federico.ferri@cern.ch>
2016-01-31 18:05:21 +0100

    first modified version of a diphoton dumper

--- Taggers/test/diphotonsDumper_cfg.py
+++ Taggers/test/diphotonsDumper_cfg.py
@@ -21,10 +21,11 @@ process.TFileService = cms.Service("TFileService",
 from flashgg.Taggers.flashggPreselectedDiPhotons_cfi import flashggPreselectedDiPhotons
 process.kinPreselDiPhotons = flashggPreselectedDiPhotons.clone(
 cut=cms.string(
-        "leadingPhoton.pt > 40 && subLeadingPhoton.pt > 30"
-        " && abs(leadingPhoton.superCluster.eta)<2.5 && abs(subLeadingPhoton.superCluster.eta)<2.5 "
-        " && ( abs(leadingPhoton.superCluster.eta)<1.4442 || abs(leadingPhoton.superCluster.eta)>1.566)"
-        " && ( abs(subLeadingPhoton.superCluster.eta)<1.4442 || abs(subLeadingPhoton.superCluster.eta)>1.566)"
+        ""
+        ##"leadingPhoton.pt > 40 && subLeadingPhoton.pt > 30"
+        ##" && abs(leadingPhoton.superCluster.eta)<2.5 && abs(subLeadingPhoton.superCluster.eta)<2.5 "
+        ##" && ( abs(leadingPhoton.superCluster.eta)<1.4442 || abs(leadingPhoton.superCluster.eta)>1.566)"
+        ##" && ( abs(subLeadingPhoton.superCluster.eta)<1.4442 || abs(subLeadingPhoton.superCluster.eta)>1.566)"
         )
                                                               )
 
@@ -55,21 +56,32 @@ cfgTools.addCategory(process.diphotonDumper,
 cfgTools.addCategories(process.diphotonDumper,
                        ## categories definition
                        ## cuts are applied in cascade. Events getting to these categories have already failed the "Reject" selection
-                       [("EBHighR9","max(abs(leadingPhoton.superCluster.eta),abs(leadingPhoton.superCluster.eta))<1.4442"
-                         "&& min(leadingPhoton.r9,subLeadingPhoton.r9)>0.94",0), ## EB high R9
-                        ("EBLowR9","max(abs(leadingPhoton.superCluster.eta),abs(leadingPhoton.superCluster.eta))<1.4442",0), ## remaining EB is low R9
-                        ("EEHighR9","min(leadingPhoton.r9,subLeadingPhoton.r9)>0.94",0), ## then EE high R9
-                        ("EELowR9","1",0), ## evereything elese is EE low R9
-                        ],
+                       #[("EBHighR9","max(abs(leadingPhoton.superCluster.eta),abs(leadingPhoton.superCluster.eta))<1.4442"
+                       #  "&& min(leadingPhoton.r9,subLeadingPhoton.r9)>0.94",0), ## EB high R9
+                       # ("EBLowR9","max(abs(leadingPhoton.superCluster.eta),abs(leadingPhoton.superCluster.eta))<1.4442",0), ## remaining EB is low R9
+                       # ("EEHighR9","min(leadingPhoton.r9,subLeadingPhoton.r9)>0.94",0), ## then EE high R9
+                       # ("EELowR9","1",0), ## evereything elese is EE low R9
+                       # ],
+                       [],
                        ## variables to be dumped in trees/datasets. Same variables for all categories
                        ## if different variables wanted for different categories, can add categorie one by one with cfgTools.addCategory
-                       variables=["CMS_hgg_mass[320,100,180]:=mass", 
-                                  "leadPt                   :=leadingPhoton.pt",
-                                  "subleadPt                :=subLeadingPhoton.pt",
-                                  "minR9                    :=min(leadingPhoton.r9,subLeadingPhoton.r9)",
-                                  "maxEta                   :=max(abs(leadingPhoton.superCluster.eta),abs(leadingPhoton.superCluster.eta))",
-                                  "leadIDMVA                :=leadingView.phoIdMvaWrtChosenVtx",
-                                  "subleadIDMVA             :=subLeadingView.phoIdMvaWrtChosenVtx",
+                       variables=["CMS_hgg_mass[320,100,180]  := mass", 
+                                  "lead_pt                    := leadingPhoton.pt",
+                                  "sublead_pt                 := subLeadingPhoton.pt",
+                                  "minR9                      := min(leadingPhoton.r9,subLeadingPhoton.r9)",
+                                  "maxEta                     := max(abs(leadingPhoton.superCluster.eta),abs(leadingPhoton.superCluster.eta))",
+                                  "lead_IdMvaWrtChosenVtx     := leadingView.phoIdMvaWrtChosenVtx",
+                                  "sublead_IdMvaWrtChosenVtx  := subLeadingView.phoIdMvaWrtChosenVtx",
+                                  "lead_R9                    := leadingPhoton.full5x5_r9",
+                                  "lead_egChargedHadronIso    := leadingPhoton.egChargedHadronIso",
+                                  "sublead_egChargedHadronIso := subLeadingPhoton.egChargedHadronIso",
+                                  "subleadR9                  := subLeadingPhoton.full5x5_r9",
+                                  "l_scEta                    := leadingPhoton.superCluster.eta",
+                                  "s_scEta                    := subLeadingPhoton.superCluster.eta",
+                                  "l_phoID                    := leadPhotonId",
+                                  "s_phoID                    := subLeadPhotonId",
+                                  "l_passElVeto               := leadingPhoton.passElectronVeto",
+                                  "s_passElVeto               := subLeadingPhoton.passElectronVeto",
                                   ],
                        ## histograms to be plotted. 
                        ## the variables need to be defined first
@@ -88,6 +100,6 @@ process.p1 = cms.Path(
 
 
 from flashgg.MetaData.JobConfig import customize
-customize.setDefault("maxEvents",100)
+customize.setDefault("maxEvents", 100)
+customize.setDefault("targetLumi", ))
 customize(process)
-
